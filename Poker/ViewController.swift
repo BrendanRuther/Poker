cd904b75d71201f552bd8eedf48b03761ea9b418
Brendan Rutherford <brendan@node-1w7jr9qheqdy0qppiln4ak3u5.ipv6.telus.net>
2020-03-21 19:51:12 -0700

    Initial Commit

---
+++ Poker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poker
+//
+//  Created by Brendan Rutherford on 2020-03-21.
+//  Copyright © 2020 Brendan Rutherford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
